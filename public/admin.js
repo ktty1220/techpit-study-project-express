351b0a10616f26796f3383886044412e53df9438
ktty1220 <ktty1220@gmail.com>
2021-03-16 13:07:36 +0900

    5-12

--- public/admin.js
+++ public/admin.js
@@ -1,3 +1,4 @@
+// 記事削除
 const deleteLinks = document.querySelectorAll('[data-delete]');
 const deleteForm = document.querySelector('form[name=delete]');
 const deleteTarget = deleteForm.querySelector('input[name=date]');
@@ -10,4 +11,31 @@ deleteLinks.forEach((link) => {
       deleteForm.submit();
     }
   });
+});
+
+// パスワード変更
+const passwordButton = document.querySelector('input[name=change_password]');
+const password = document.querySelector('input[name=password]');
+const passwordVerify = document.querySelector('input[name=password_verify]');
+
+passwordButton.addEventListener('click', (ev) => {
+  const fetchPromise = fetch('/admin/change_password', {
+    method: 'POST',
+    body: 'password=' + password.value + '&password_verify=' + passwordVerify.value,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  });
+
+  fetchPromise
+    .then((response) => {
+      return response.text();
+    })
+    .then((message) => {
+      alert(message);
+    })
+    .catch((err) => {
+      console.error(err);
+      alert('パスワードの変更ができませんでした。');
+    });
 });
