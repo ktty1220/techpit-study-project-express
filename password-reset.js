d63e0d9fd4c30b9c377705de1dc4cb5ee251b280
ktty1220 <ktty1220@gmail.com>
2021-03-16 13:07:49 +0900

    5-15

--- password-reset.js
+++ password-reset.js
@@ -2,10 +2,8 @@
 const cryptoRandomString = require('crypto-random-string');
 // パスワードハッシュ化パッケージを読み込み
 const bcrypt = require('bcryptjs');
-// Node.js内蔵のファイル操作モジュールを読み込み
-const fs = require('fs');
-// Node.js内蔵のファイルパス関連モジュールを読み込み
-const path = require('path');
+// 同じフォルダにあるfunctions.jsを読み込み
+const func = require('./functions');
 
 // 初期パスワードを生成(ランダム8文字)
 const initPassword = cryptoRandomString({
@@ -13,5 +11,5 @@ const initPassword = cryptoRandomString({
 });
 // ハッシュ化パスワードを作成して保存
 const hashed = bcrypt.hashSync(initPassword);
-fs.writeFileSync(path.join(__dirname, '.password'), hashed);
+func.savePassword(hashed);
 console.log('パスワードを初期化しました。: ' + initPassword);
