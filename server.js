aa71cc4a5829a31714da44eb2f4ff17c9a1da04f
ktty1220 <ktty1220@gmail.com>
2021-05-25 14:54:44 +0900

    9-15

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.get('/blog/:date', (request, response) => {
 app.post('/blog/:date/post_comment', (request, response) => {
   const { date } = request.params;
   const { comment } = request.body;
-  if (comment) {
+  if (comment && request.body.captcha === request.session.captcha) {
     func.saveComment(date, comment);
   }
   response.redirect('/blog/' + date);
