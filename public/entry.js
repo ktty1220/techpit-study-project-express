7280d6fef2cbf56bfe78b4757f3fe11779e538a8
ktty1220 <ktty1220@gmail.com>
2021-06-21 21:26:26 +0900

    9-14

--- public/entry.js
+++ public/entry.js
@@ -1,14 +1,25 @@
 const captchaImage = document.querySelector('.captcha-image');
+const captchaRefreshLink = document.querySelector('.captcha-message a');
 
 // CAPTCHA画像表示
-fetch('/captcha_image')
-  .then(function (response) {
-    return response.text();
-  })
-  .then(function (svg) {
-    captchaImage.innerHTML = svg;
-  })
-  .catch(function (err) {
-    console.error(err);
-    alert('認証用画像を取得できません。');
-  });
+function loadCaptchaImage() {
+  fetch('/captcha_image?' + Date.now())
+    .then(function (response) {
+      return response.text();
+    })
+    .then(function (svg) {
+      captchaImage.innerHTML = svg;
+    })
+    .catch(function (err) {
+      console.error(err);
+      alert('認証用画像を取得できません。');
+    });
+}
+
+loadCaptchaImage();
+
+// CAPTCHA画像変更
+captchaRefreshLink.addEventListener('click', function (ev) {
+  ev.preventDefault();
+  loadCaptchaImage();
+});
